e3e03026064f9e5e0f6c8618e30c670e5ec96c37
Han <s3876531@gmail.com>
2020-04-12 23:01:25 +0800

    Setup CoreData Make the inputView of train frequency  become picker NEW Entity:User  Attributre: name, age, weight, height, train frequency

--- fitube/Controllers/RegisterViewController.swift
+++ fitube/Controllers/RegisterViewController.swift
@@ -13,20 +13,26 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    var safeEmail : String?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     @IBAction func registerButtonPressed(_ sender: UIButton) {
-//        if let newEmail = emailTextField.text, let newPass = passwordTextField.text{
+        if let newEmail = emailTextField.text, let newPass = passwordTextField.text{
 //            Auth.auth().createUser(withEmail: newEmail, password: newPass) { (authResult, error) in
 //                if let e = error{
 //                    print("Register's error: \(e)")
 //                }else{
                     performSegue(withIdentifier: "registerDetail", sender: self)
+                    safeEmail = newEmail
 //                }
 //            }
-//        }
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as? UserDataController
+        destinationVC!.userEmail = safeEmail!
     }
 }
