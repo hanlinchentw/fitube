7b79700e90a24f1eeba0536e57aff4001e9732bb
Han <s3876531@gmail.com>
2020-04-17 18:42:17 +0800

    Set up the IBOutlet of label in Train View

--- fitube/Controllers/Program/TrainViewController.swift
+++ fitube/Controllers/Program/TrainViewController.swift
@@ -11,10 +11,21 @@ import UIKit
 class TrainViewController: UIViewController {
     
     var trainingSection : [String]?
+    @IBOutlet weak var exerciseLabel: UILabel!
+    @IBOutlet weak var remainingSet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
       
+    }
+    
+    @IBAction func previousOneButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func nextOneButton(_ sender: UIButton) {
+        
+        
     }
     
 
