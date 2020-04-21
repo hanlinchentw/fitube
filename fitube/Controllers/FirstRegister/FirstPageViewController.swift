2733dfd829679e3647325bf253df305067ed3d10
Han <s3876531@gmail.com>
2020-04-21 23:37:48 +0800

    start to build auto layout

--- fitube/Controllers/FirstRegister/FirstPageViewController.swift
+++ fitube/Controllers/FirstRegister/FirstPageViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class FirstPageViewController: UIViewController {
 
+    @IBOutlet weak var welcomeStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NSLayoutConstraint(item: welcomeStackView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
        
+        NSLayoutConstraint(item: welcomeStackView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+        
     }
     
 
--- fitube/Controllers/FirstRegister/SecondPageViewController.swift
+++ fitube/Controllers/FirstRegister/SecondPageViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class SecondPageViewController: UIViewController {
 
+    @IBOutlet weak var textView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet var textLable: [UILabel]!
+    
+    @IBOutlet weak var textStack: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textStack.distribution = .fillEqually
+        textStack.spacing = 20
+        textView.layer.cornerRadius = 20
+        
+        NSLayoutConstraint(item: titleLabel!, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: titleLabel!, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: view.frame.height/8).isActive = true
+        
+        NSLayoutConstraint(item: textStack!, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: textStack!, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: textStack!, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 20).isActive = true
+        NSLayoutConstraint(item: textStack!, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 20).isActive = true
+        
+        NSLayoutConstraint(item: textView!, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 10).isActive = true
+        NSLayoutConstraint(item: textView!, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 10).isActive = true
+        NSLayoutConstraint(item: textView!, attribute: .centerX, relatedBy: .equal, toItem: textStack, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: textView!, attribute: .centerY, relatedBy: .equal, toItem: textStack, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+        
+        for n in 0...(textLable.count-1){
+            textLable[n].textColor = .white
+            NSLayoutConstraint(item: textLable[n], attribute: .leading, relatedBy: .equal, toItem: textStack, attribute: .leading, multiplier: 1, constant: 3).isActive = true
+            NSLayoutConstraint(item: textLable[n], attribute: .trailing, relatedBy: .equal, toItem: textStack, attribute: .trailing, multiplier: 1, constant: 3).isActive = true
+        }
     }
     
 
